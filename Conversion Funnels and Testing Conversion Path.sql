6e25ca58784cb3cf07b2fdf8d117faa4e08f419a
JC Ketterer <jcketterer18@gmail.com>
2020-11-28 17:04:29 -0700

    mid-course project

--- Conversion Funnels and Testing Conversion Path.sql
+++ Conversion Funnels and Testing Conversion Path.sql
@@ -114,6 +114,131 @@ SELECT
     COUNT(DISTINCT CASE WHEN cart_madeit = 1 THEN website_session_id ELSE NULL END) 
 		/COUNT(DISTINCT CASE WHEN mrfuzzy_madeit = 1 THEN website_session_id ELSE NULL END) AS click_to_cart
 FROM session_level_madeit_flags
+
+-- Building Conversion Funnels 
+
+-- STEP 1: select all pageviews for relevant sessions 
+-- STEP 2: ID each pageview as the specific funnel step
+-- STEP 3: create the session-level conversion funnel view 
+-- STEP 4: Aggregate the data to assess funnel performance 
+
+SELECT 
+	website_sessions.website_session_id,
+    website_pageviews.pageview_url,
+    -- website_pageviews.created_at AS pageview_created_at,
+    CASE WHEN pageview_url = '/products' THEN 1 ELSE 0 END AS products_page,
+    CASE WHEN pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS mrfuzzy_page,
+    CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page,
+    CASE WHEN pageview_url = '/shipping' THEN 1 ELSE 0 END AS shipping_page,
+    CASE WHEN pageview_url = '/billing' THEN 1 ELSE 0 END AS billing_page,
+    CASE WHEN pageview_url = '/thank-you-for-your-order' THEN 1 ELSE 0 END AS thankyou_page
+FROM website_sessions
+	LEFT JOIN website_pageviews
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE website_sessions.utm_source = 'gsearch'
+	AND website_sessions.utm_campaign = 'nonbrand'
+    AND website_sessions.created_at > '2012-08-05'
+    AND website_sessions.created_at < '2012-09-05'
+ORDER BY
+	website_sessions.website_session_id,
+    website_pageviews.created_at;
     
+-- Wrap the above query into a subquery 
+
+SELECT 
+	website_session_id,
+    MAX(products_page) AS product_madeit,
+	MAX(mrfuzzy_page) AS mrfuzzy_madeit,
+	MAX(cart_page) AS cart_madeit,
+    MAX(shipping_page) AS shipping_madeit,
+    MAX(billing_page) AS billing_madeit,
+    MAX(thankyou_page) AS thankyou_madeit
+FROM(
+	SELECT 
+	website_sessions.website_session_id,
+    website_pageviews.pageview_url,
+    -- website_pageviews.created_at AS pageview_created_at,
+    CASE WHEN pageview_url = '/products' THEN 1 ELSE 0 END AS products_page,
+    CASE WHEN pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS mrfuzzy_page,
+    CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page,
+    CASE WHEN pageview_url = '/shipping' THEN 1 ELSE 0 END AS shipping_page,
+    CASE WHEN pageview_url = '/billing' THEN 1 ELSE 0 END AS billing_page,
+    CASE WHEN pageview_url = '/thank-you-for-your-order' THEN 1 ELSE 0 END AS thankyou_page
+FROM website_sessions
+	LEFT JOIN website_pageviews
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE website_sessions.utm_source = 'gsearch'
+	AND website_sessions.utm_campaign = 'nonbrand'
+    AND website_sessions.created_at > '2012-08-05'
+    AND website_sessions.created_at < '2012-09-05'
+ORDER BY
+	website_sessions.website_session_id,
+    website_pageviews.created_at
+) AS pageview_level
+GROUP BY
+	website_session_id;
+
+-- Create a temp table
+CREATE TEMPORARY TABLE session_level_made_it_flags
+SELECT 
+	website_session_id,
+    MAX(products_page) AS product_madeit,
+	MAX(mrfuzzy_page) AS mrfuzzy_madeit,
+	MAX(cart_page) AS cart_madeit,
+    MAX(shipping_page) AS shipping_madeit,
+    MAX(billing_page) AS billing_madeit,
+    MAX(thankyou_page) AS thankyou_madeit
+FROM(
+	SELECT 
+	website_sessions.website_session_id,
+    website_pageviews.pageview_url,
+    -- website_pageviews.created_at AS pageview_created_at,
+    CASE WHEN pageview_url = '/products' THEN 1 ELSE 0 END AS products_page,
+    CASE WHEN pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS mrfuzzy_page,
+    CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page,
+    CASE WHEN pageview_url = '/shipping' THEN 1 ELSE 0 END AS shipping_page,
+    CASE WHEN pageview_url = '/billing' THEN 1 ELSE 0 END AS billing_page,
+    CASE WHEN pageview_url = '/thank-you-for-your-order' THEN 1 ELSE 0 END AS thankyou_page
+FROM website_sessions
+	LEFT JOIN website_pageviews
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE website_sessions.utm_source = 'gsearch'
+	AND website_sessions.utm_campaign = 'nonbrand'
+    AND website_sessions.created_at > '2012-08-05'
+    AND website_sessions.created_at < '2012-09-05'
+ORDER BY
+	website_sessions.website_session_id,
+    website_pageviews.created_at
+) AS pageview_level
+GROUP BY
+	website_session_id;
+
+SELECT * FROM session_level_made_it_flags
+
+-- Final output 1 
+
+SELECT 
+	COUNT(DISTINCT website_session_id) AS sessions, 
+    COUNT(DISTINCT CASE WHEN product_madeit = 1 THEN website_session_id ELSE NULL END) AS to_products,
+    COUNT(DISTINCT CASE WHEN mrfuzzy_madeit = 1 THEN website_session_id ELSE NULL END) AS to_mrfuzzy,
+    COUNT(DISTINCT CASE WHEN cart_madeit = 1 THEN website_session_id ELSE NULL END) AS to_cart,
+    COUNT(DISTINCT CASE WHEN shipping_madeit = 1 THEN website_session_id ELSE NULL END) AS to_shipping,
+    COUNT(DISTINCT CASE WHEN billing_madeit = 1 THEN website_session_id ELSE NULL END) AS to_billing,
+    COUNT(DISTINCT CASE WHEN thankyou_madeit = 1 THEN website_session_id ELSE NULL END) AS to_thankyou
+FROM session_level_made_it_flags;
+
+-- final output 2 click rates
+
+SELECT 
+	COUNT(DISTINCT CASE WHEN product_madeit = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT website_session_id) AS lander_click_rt, 
+    COUNT(DISTINCT CASE WHEN mrfuzzy_madeit = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN product_madeit = 1 THEN website_session_id ELSE NULL END) AS to_mrfuzzy,
+    COUNT(DISTINCT CASE WHEN cart_madeit = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN mrfuzzy_madeit = 1 THEN website_session_id ELSE NULL END) AS to_cart,
+    COUNT(DISTINCT CASE WHEN shipping_madeit = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN cart_madeit = 1 THEN website_session_id ELSE NULL END) AS to_shipping,
+    COUNT(DISTINCT CASE WHEN billing_madeit = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN shipping_madeit = 1 THEN website_session_id ELSE NULL END) AS to_billing,
+    COUNT(DISTINCT CASE WHEN thankyou_madeit = 1 THEN website_session_id ELSE NULL END)/COUNT(DISTINCT CASE WHEN billing_madeit = 1 THEN website_session_id ELSE NULL END) AS to_thankyou
+FROM session_level_made_it_flags;
+    
+    
+
     
     
